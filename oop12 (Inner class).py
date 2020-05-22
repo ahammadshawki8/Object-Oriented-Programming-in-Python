232dcf95a80ad8d825827cda26c7fcfea455456e
ahammadshawki8 <ahammadshawki8@gmail.com>
2020-05-22 13:28:33 +0600

    5/22/20

--- oop12 (Inner class).py
+++ oop12 (Inner class).py
@@ -18,7 +18,7 @@ class Student:
         # we can do this if we pass the attributes when creating a student class.
         
     def show(self):
-        print(f"STUDENT INFORMATION\nname : {self._name}\nroll : {self._roll}")
+        print(f"STUDENT INFORMATION \n\t name : {self._name} \n\t roll : {self._roll}")
 
     # students also have a laptop, so we want a laptop attribute in this class.
     # but the problem is when we talk about laptop,
@@ -33,7 +33,7 @@ class Student:
             self.ram=ram
 
         def show(self):
-            print(f"LAPTOP CONFIGURATION\nbrand : {self.brand}\ncpu : {self.cpu}\nram : {self.ram}")
+            print(f"LAPTOP CONFIGURATION \n\t brand : {self.brand} \n\t cpu : {self.cpu} \n\t ram : {self.ram}")
             
             
     # to create the object of the inner laptop class in the outer class, we can do that in the __init__ method.
@@ -44,17 +44,21 @@ s2=Student("Arko",5162)
 
 s1.show()
 
-# creating Laptop object for students
-s1_lap=s1.Laptop("Lenovo","i5",4)
-s2_lap=s2.Laptop("HP","i3",2)
+# creating Laptop object for students. 
+s1.lap=s1.Laptop("Lenovo","i5",4)
+s2.lap=s2.Laptop("HP","i3",2)
+# this object will be an attribute of the student object.
 
 # we can print the attributes of Laptop using,
-print(s1_lap.brand)
+print(s1.lap.brand)
 
-# we haev two different classes with the same name. but we can access both of them.
+# we have two different classes with the same name. but we can access both of them.
 print()
 s1.show()
 print()
-s1_lap.show()
+s1.lap.show()
 
 # NOTE: it is not simmilar to inheritance.
+# it can reduce potential name conflicts because it allows for a simmilarly named class to exists in another context.
+# another advantage is that it allows for a more advanced form of inheritance
+# in which a subclass of the outerclass can override the defination of its inner class.
